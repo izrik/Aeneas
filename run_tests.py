342bd31683cc0de316f48a97f0f9d59b3be9b836
izrik <izrik@izrik.com>
2015-10-06 09:40:14 -0500

    Basic test running.

--- run_tests.py
+++ run_tests.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python2
 
+import unittest
+import argparse
+import logging
+
+
+def run():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--print-log', action='store_true',
+                        help='Print the log.')
+    args = parser.parse_args()
+
+    if args.print_log:
+        logging.basicConfig(level=logging.DEBUG,
+                            format=('%(asctime)s %(levelname)s:%(name)s:'
+                                    '%(funcName)s:'
+                                    '%(filename)s(%(lineno)d):'
+                                    '%(threadName)s(%(thread)d):%(message)s'))
+
+    unittest.main(argv=[''])
+
 if __name__ == '__main__':
-    print('run_tests.py')
+    run()
