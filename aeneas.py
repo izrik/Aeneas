e9370640deb581c50ef7a705c2d4837443e1e637
izrik <izrik@izrik.com>
2015-10-06 09:16:28 -0500

    Basic start-up and dependencies.

--- aeneas.py
+++ aeneas.py
@@ -1,4 +1,40 @@
 #!/usr/bin/env python2
 
+import argparse
+from os import environ
+
+
+def bool_from_str(s):
+    if isinstance(s, basestring):
+        s = s.lower()
+    if s in ['true', 't', '1', 'y']:
+        return True
+    if s in ['false', 'f', '0', 'n']:
+        return False
+    return bool(s)
+
+
+AENEAS_DEBUG = bool_from_str(environ.get('AENEAS_DEBUG', False))
+
+DEFAULT_AENEAS_PORT = 4935
+AENEAS_PORT = environ.get('AENEAS_PORT', DEFAULT_AENEAS_PORT)
+try:
+    AENEAS_PORT = int(AENEAS_PORT)
+except:
+    AENEAS_PORT = DEFAULT_AENEAS_PORT
+
+
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--debug', help='Run Flask in debug mode, with '
+                                        'auto-reload and debugger page on '
+                                        'errors.',
+                        action='store_true', default=AENEAS_DEBUG)
+    parser.add_argument('--port', help='The port on which to accept incoming '
+                                       'HTTP requests. Default is {}'.format(
+                                            AENEAS_PORT),
+                        action='store', default=AENEAS_PORT, type=int)
+
+    args = parser.parse_args()
+
     print('aeneas.py')
